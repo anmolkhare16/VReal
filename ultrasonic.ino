48bf62d8d1994d447ddd52d898fa362d142f86c9
anmolkhare16 <anmolkhare16@gmail.com>
2017-02-17 21:44:14 +0530

    Ultrasonic
    this code is for controlling servo motor using ultrasonic.

---
+++ ultrasonic.ino
@@ -0,0 +1,65 @@
+const int trigPin = 2;
+const int echoPin = 4;
+#include <Servo.h>
+Servo myservo;
+int p=0;
+int c=0;
+void setup() {
+ myservo.attach(9);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+  long duration, inches, cm;
+
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+
+  inches = microsecondsToInches(duration);
+  cm = microsecondsToCentimeters(duration);
+
+  if(cm<=5 || (cm>=8 && cm<=10)){
+    //while(cm<=5 || (cm>=8 && cm<=10)){
+c=c+1;
+   
+    
+    if(c==1){
+       myservo.write(30);
+    }
+    
+   //  p=p+1;
+   }
+  else{
+     c&=0;
+     myservo.write(0);
+  }
+  
+  //Serial.print(inches);
+ // Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(100);
+}
+
+long microsecondsToInches(long microseconds)
+{
+  return microseconds / 74 / 2;
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
